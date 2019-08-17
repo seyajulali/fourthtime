2f82f6d97cbe8667d5ddb67709798832a05fddf0
seyajulali <53977451+seyajulali@users.noreply.github.com>
2019-08-17 14:35:23 +0530

    print changed ali

--- s.py
+++ s.py
@@ -3,7 +3,10 @@ for i in range(1,6,1):
         if i==1 or i==3 or i==5 or i+j==3 or (i+j==9 and(i<j)):
                                                        
             print("*",end="")
-    print()            
+    print()    
+    
+    print("ali")
+    print("seyajul")
             
 
                                                                 
